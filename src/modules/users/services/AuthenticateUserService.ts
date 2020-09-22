c0999b08190d75e6c9625ba0f134a996128bc15c
jlimadev <jlima.dev@gmail.com>
2020-09-22 16:14:02 -0300

    added implementation of hashProvider with dependency injection and dependency inversion

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -2,9 +2,9 @@ import authConfig from '@config/auth';
 import UserEntity from '@modules/users/infra/typeorm/entities/UserEntity';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequestDTO {
   email: string;
@@ -21,6 +21,7 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({
@@ -33,7 +34,10 @@ class AuthenticateUserService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,8 @@
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import UserEntity from '@modules/users/infra/typeorm/entities/UserEntity';
 import AppError from '@shared/errors/AppError';
-import { hash } from 'bcryptjs';
 import { injectable, inject } from 'tsyringe';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 @injectable()
@@ -10,6 +10,9 @@ class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({
@@ -23,7 +26,7 @@ class CreateUserService {
       throw new AppError('This e-mail is already in use.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.hashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
