b9ce13b42f69815270899769d872856b5e6a9b30
jlimadev <jlima.dev@gmail.com>
2020-10-06 11:59:42 -0300

    added template for email using handlebars

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,4 @@
-// import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
-// import UserEntity from '@modules/users/infra/typeorm/entities/UserEntity';
+import path from 'path';
 import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
@@ -32,10 +31,27 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de Recuperação de senha recebido, ${token}`,
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
     );
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `https://localhost:3001/reset_password?token=${token}`,
+        },
+      },
+    });
   }
 }
 
