31c13e27e970775d818853d8b9ed3399ea9b4376
jlimadev <jlima.dev@gmail.com>
2020-10-13 17:24:11 -0300

    FEAT04RNO4 - O usuário não deve poder agendar em horário que já passou

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequestDTO {
@@ -43,14 +43,18 @@ class ListProviderDayAvailabityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourDay.map(hour => {
       const hasAppointmentInHour = appointments.find(appointment => {
         return getHours(appointment.date) === hour;
       });
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
 
