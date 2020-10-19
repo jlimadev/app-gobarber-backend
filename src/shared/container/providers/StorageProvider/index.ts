d0c42f73a358e4b1cc8d148eb3c729d5246a7cb5
jlimadev <jlima.dev@gmail.com>
2020-10-19 17:34:43 -0300

    organized tsryinge containers

---
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -0,0 +1,12 @@
+import { container } from 'tsyringe';
+import IStorageProvider from './models/IStorageProviders';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+
+const providers = {
+  disk: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers.disk,
+);
