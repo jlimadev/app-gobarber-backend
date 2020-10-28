c51c4fcc262228efd78b1e94009e04df6c25ea12
jlimadev <jlima.dev@gmail.com>
2020-10-28 12:09:53 -0300

    adjusted order of middlewares to avoid cors errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -15,8 +15,8 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
+app.use(rateLimiter);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
